ad9e01fa0533118ce7baf15e4e99abcb5bb46036
Ahmed Khodier <70490707+Ahmedkhodier-code@users.noreply.github.com>
2022-04-25 03:38:02 +0200

    new update

--- app/src/main/java/sci/khodier/andriod/elearningdemo/notification/MyFirebaseServices.java
+++ app/src/main/java/sci/khodier/andriod/elearningdemo/notification/MyFirebaseServices.java
@@ -3,7 +3,9 @@ package sci.khodier.andriod.elearningdemo.notification;
 
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.util.Log;
+import android.widget.Toast;
+import com.github.arturogutierrez.Badges;
+import com.github.arturogutierrez.BadgesNotSupportedException;
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -16,14 +18,14 @@ import sci.khodier.andriod.elearningdemo.R;
 public class MyFirebaseServices extends FirebaseMessagingService {
 
     private static final String TAG = "PushNotification";
-    private static final String CHANNEL_ID ="101" ;
+    private static final String CHANNEL_ID = "101";
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-        showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+        showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
     }
 
-    private void showNotification(String title,String message){
+    private void showNotification(String title, String message) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
@@ -36,11 +38,15 @@ public class MyFirebaseServices extends FirebaseMessagingService {
                 // Set the intent that will fire when the user taps the notification
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
+        try {
+            Badges.setBadge(getBaseContext(), 1);
+        } catch (BadgesNotSupportedException error) {
+            Toast.makeText(getBaseContext(), "That was a error!", Toast.LENGTH_SHORT).show();
 
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
-        // notificationId is a unique int for each notification that you must define
-        notificationManager.notify(1, builder.build());
+            // notificationId is a unique int for each notification that you must define
+            notificationManager.notify(1, builder.build());
+        }
     }
 }
-
